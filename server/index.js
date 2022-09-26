7d9d32a06e994cb89cca1b5e01c7a7164b4c705c
sarthaksavvy <sarthak@bitfumes.com>
2022-09-26 19:27:29 +0530

    04 MERN with Socket.io - first socket connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 import express from "express";
-
+import http from "http";
+import { Server } from "socket.io";
 const app = express();
 const PORT = 4000;
 
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.get("/", (req, res) => {
-  res.json({ data: "hello world from socket" });
+  // res.json({ data: "hello world from socket" });
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("Connection is ready");
+});
+
+httpServer.listen(PORT, () => {
   console.log("Server is running at http://localhost:4000");
 });
