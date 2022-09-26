d707edcb37418309671ef3f1aa56d5939b4b6bf5
sarthaksavvy <sarthak@bitfumes.com>
2022-09-26 22:19:10 +0530

    09 MERN with Socket.io - message box design

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -1,5 +1,6 @@
 import SendIcon from "@mui/icons-material/Send";
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import OutlinedInput from "@mui/material/OutlinedInput";
@@ -20,39 +21,57 @@ export default function ChatWindow() {
     if (!socket) return;
 
     socket.on("message-from-server", (data) => {
-      setChat((prev) => [...prev, data.message]);
+      setChat((prev) => [...prev, { message: data.message, received: true }]);
     });
   }, [socket]);
 
   function handleForm(e) {
     e.preventDefault();
     socket.emit("send-message", { message });
+    setChat((prev) => [...prev, { message, received: false }]);
     setMessage("");
   }
 
   return (
-    <>
-      <Box sx={{ marginBottom: 5 }}>
-        {chat.map((message) => (
-          <Typography key={message}>{message}</Typography>
-        ))}
-      </Box>
-      <Box component="form" onSubmit={handleForm}>
-        <OutlinedInput
-          label="Write your message"
-          size="small"
-          value={message}
-          placeholder="Write your message"
-          onChange={(e) => setMessage(e.target.value)}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton type="submit" edge="end">
-                <SendIcon />
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-      </Box>
-    </>
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Card
+        sx={{
+          padding: 2,
+          marginTop: 10,
+          width: "60%",
+          backgroundColor: "gray",
+          color: "white",
+        }}
+      >
+        <Box sx={{ marginBottom: 5 }}>
+          {chat.map((data) => (
+            <Typography
+              sx={{ textAlign: data.received ? "left" : "right" }}
+              key={data.message}
+            >
+              {data.message}
+            </Typography>
+          ))}
+        </Box>
+        <Box component="form" onSubmit={handleForm}>
+          <OutlinedInput
+            sx={{ backgroundColor: "white" }}
+            size="small"
+            fullWidth
+            value={message}
+            placeholder="Write your message"
+            inputProps={{ "aria-label": "search google maps" }}
+            onChange={(e) => setMessage(e.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton type="submit" edge="end">
+                  <SendIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </Box>
+      </Card>
+    </Box>
   );
 }
